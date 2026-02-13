ffb8c9e7572846e2cc44eb85e850a363b315ce45
Gabriel de Miranda <demirand@mit.edu>
2026-02-12 19:11:50 -0800

    add script for compiling mliap and torchscript models for baseline measurement

---
+++ models/compile_for_baseline.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH -J lmp_compile
+#SBATCH -p gpu
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH -t 00:10:00
+#SBATCH --mem=75GB
+#SBATCH -o logs/%j.err
+#SBATCH -e logs/%j.out
+#SBATCH --account m5156
+#SBATCH --constraint="gpu&hbm80g"
+
+
+source ~/.bashrc
+
+# Laod modules
+module load cmake/3.30.2
+module load cudatoolkit/12.9
+module load craype-accel-nvidia80
+module load cudnn/9.5.0
+module list
+
+# Activate micromamba env
+micromamba activate mliap_benchmark_2025
+
+# Get model zip 
+wget https://zenodo.org/api/records/16980200/files/NequIP-OAM-L-0.1.nequip.zip/content \
+     -O NequIP-OAM-L-0.1.nequip.zip
+
+# Job working directory (where sbatch was run from)
+WORK_DIR=$(pwd)
+
+mkdir -p compiled_models
+nequip-prepare-lmp-mliap \
+	"${WORK_DIR}/NequIP-OAM-L-0.1.nequip.zip" \
+	"${WORK_DIR}/compiled_models/NequIP-L-nocompile-a100.nequip.lmp.pt" \
+	--no-compile
+
+nequip-compile \
+	"${WORK_DIR}/NequIP-OAM-L-0.1.nequip.zip" \
+	"${WORK_DIR}/compiled_models/NequIP-L-torchscript-a100.nequip.pth" \
+	--device cuda \
+	--mode torchscript 
